9d65e55bdb5927a60be7323504e3375a41590462
Bruno Riwerson Silva <riwerson@gmail.com>
2021-12-27 19:25:33 -0300

    Light and Dark mode functions done

---
+++ 05.light-and-dark-mode/socials.js
@@ -0,0 +1,7 @@
+const socialLinks = {
+    github:'https://github.com/vanriwerson',
+    linkedin: 'www.linkedin.com/in/bruno-riwerson',
+    facebook: 'https://m.facebook.com/vanriwerson',
+    instagram: 'https://www.instagram.com/vanriwerson/',
+    twitter: 'https://twitter.com/BrunoRiwerson',
+}
