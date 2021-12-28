b603a478a27dfb5018d67de8fe945de4b2be27b0
Bruno Riwerson Silva <riwerson@gmail.com>
2021-12-28 09:51:07 -0300

    refactoring code and saving users preference

--- 05.light-and-dark-mode/script.js
+++ 05.light-and-dark-mode/script.js
@@ -6,12 +6,20 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
+function changeImageMode(color) {
+  image1.src = `./img/undraw_proud_coder_${color}.svg`;
+  image2.src = `./img/undraw_feeling_proud_${color}.svg`;
+  image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
+}
+
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem('theme', 'dark');
     darkMode()
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem('theme', 'light');
     lightMode()
   }
 }
@@ -20,22 +28,28 @@ function darkMode() {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   toggleIcon.children[0].textContent = 'to Light Mode';
-  toggleIcon.children[1].classList.remove('fa-moon');
-  toggleIcon.children[1].classList.add('fa-sun');
-  image1.src = './img/undraw_proud_coder_dark.svg';
-  image2.src = './img/undraw_feeling_proud_dark.svg';
-  image3.src = './img/undraw_conceptual_idea_dark.svg';
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+
+  changeImageMode('dark')
 }
 
 function lightMode() {
   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
   toggleIcon.children[0].textContent = 'to Dark Mode';
-  toggleIcon.children[1].classList.remove('fa-sun');
-  toggleIcon.children[1].classList.add('fa-moon');
-  image1.src = './img/undraw_proud_coder_light.svg';
-  image2.src = './img/undraw_feeling_proud_light.svg';
-  image3.src = './img/undraw_conceptual_idea_light.svg';  
+  toggleIcon.children[1].classList.remove('fa-sun', 'fa-moon');
+  
+  changeImageMode('light')
 }
 
 toggleSwitch.addEventListener("change", switchTheme);
+
+const currentTheme = localStorage.getItem('theme')
+if(currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if(currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode()
+  }
+}
